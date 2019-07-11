dc3e11f7b09ede79ee87b2c4feb3dbd2d7a2e819
DeathBot <tycjan0605@gmail.com>
2019-07-11 23:59:04 +0200

    Created server for tests

--- itemshop-service/app.js
+++ itemshop-service/app.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('Hello world');
+var net = require("net");
+
+var pluginKey = "PLUGIN1";
+
+var server = net.createServer((c) => {
+    //connection
+    console.log("Someone tried to connect ... verifing");
+    c.on('end', () => {
+        console.log("Plugin disconnected");
+    });
+    c.on('data', (buffer) => {
+        if (buffer.toString() === pluginKey + " try_connection") {
+            console.log("Plugin connected to server!");
+        } else if (buffer.toString().includes(pluginKey)) {
+            console.log("Non-implemented packet: " + buffer.toString().replace(pluginKey, null));
+        } else {
+            console.log("WRONG PACKET KEY FETCHED! DISCONNECTING SOCKET!!!!");
+            c.end();
+        }
+    });
+});
+
+server.on('error', (e) => {
+    throw e;
+});
+
+server.listen(8475, () => {
+    console.log("ItemShop-Service started.");
+});
