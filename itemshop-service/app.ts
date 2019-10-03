7831583a8ab5dd8a21d02d81c2b85d26ce7753bd
Tycjan Falana <tycjan0605@gmail.com>
2019-10-03 08:19:58 +0200

    Update app.ts

--- itemshop-service/app.ts
+++ itemshop-service/app.ts
@@ -1,27 +1,6 @@
 ﻿import ISServer from "./service/ISServer";
-import ConsoleInput from "./console/ConsoleInput";
-
 
 const IS = new ISServer();
 
-IS.listen(8475);
+IS.listen(IS.Config.getServiceConfig().port);
 
-/*import express from "express";
-import MCClient from "./minecraft/MCClient";
-const app = express();
-app.use(express.static(__dirname + '/page'));
-const port = 3000;
-
-app.get('/', (req, res) => res.send());
-
-app.post('/clicked', (req, res) => {
-    const click = { clickTime: new Date() };
-    for (let i = 0; i < IS.serverList.length; i++) {
-        if (IS.serverList[i].clientName == "hellhc.pl") {
-            console.log(`[Express] Sended click event to ${IS.serverList[i].clientName}!`);
-            IS.serverList[i].socket.write(IS.serverList[i].clientKey+" He clicked me! ;-;");
-        }
-    }
-});
-
-app.listen(port, () => console.log(`Page listening on port ${port}!`))*/
