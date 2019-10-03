841d6b212b4a2af69c35ca0d74672a9ac42405d2
Tycjan Falana <tycjan0605@gmail.com>
2019-10-03 08:13:22 +0200

    Update Config.ts

--- itemshop-service/utils/Config.ts
+++ itemshop-service/utils/Config.ts
@@ -1,3 +1,13 @@
+"use strict";
 export default class Config{
+  public constructor(){
+    this._config = require("../config.json"); 
+  }
 
+  getConfig(): JSON{
+      return this._config;
+  }
+
+  private _config: JSON;
 }
+
